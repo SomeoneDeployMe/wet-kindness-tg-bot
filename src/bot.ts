4400a5f813e8df98d90ae9d358292affdd04f09c
SomeoneDeployMe <valentin.qzmin@gmail.com>
2024-03-29 20:35:49 +0300

    add basic version of /readycheck command

--- src/bot.ts
+++ src/bot.ts
@@ -2,17 +2,25 @@ import {Bot, MemorySessionStorage} from 'grammy';
 import 'dotenv/config';
 import {type ChatMember} from 'grammy/types';
 import {chatMembers} from '@grammyjs/chat-members';
-import {dota, helpme, mid, slap} from './commands';
+import {
+  dota,
+  helpme,
+  mid,
+  onReadyCheckAnswer,
+  readycheck,
+  slap,
+} from './commands';
 import {BotContext} from './types';
 
 const storageAdapter = new MemorySessionStorage<ChatMember>();
 
-const bot = new Bot<BotContext>(process.env.BOT_API_TOKEN!);
+export const bot = new Bot<BotContext>(process.env.BOT_API_TOKEN!);
 
 bot.use(chatMembers(storageAdapter));
 
 bot.api.setMyCommands([
   {command: 'start', description: 'Выразить почтение'},
+  {command: 'readycheck', description: 'Dota2 readycheck'},
   {command: 'dota', description: 'Запросить заседание по вопросам подъёма ММР'},
   {command: 'mid', description: 'Отправить бездаря на мид'},
   {command: 'slap', description: 'Дать леща @мимокроку'},
@@ -23,6 +31,7 @@ bot.command('start', async (ctx) => {
   await ctx.reply('Вечер в хату');
 });
 
+bot.command('readycheck', readycheck);
 bot.command('dota', dota);
 bot.command('mid', mid);
 bot.command('slap', slap);
@@ -36,6 +45,8 @@ bot.hears(/(?<![а-яА-Я])(спал|заснул|окуклился)(?![а-я�
   }
 });
 
+bot.on('poll_answer', onReadyCheckAnswer);
+
 bot.start({
-  allowed_updates: ['chat_member', 'message'],
+  allowed_updates: ['chat_member', 'message', 'poll_answer'],
 });
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,3 +2,4 @@ export {dota} from './dota';
 export {slap} from './slap';
 export {helpme} from './helpme';
 export {mid} from './mid';
+export {readycheck, onReadyCheckAnswer} from './readycheck';
